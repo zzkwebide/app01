4f43fcc57fa9ba969e8b330808c5f210fcfa772c
Kun Zhang <zhangkun167@gmail.com>
2016-07-21 16:06:00 +0000

    initial commit from webide

---
+++ webapp/view/main.view.js
@@ -0,0 +1,28 @@
+sap.ui.jsview("zzk.test.view.main", {
+
+	/** Specifies the Controller belonging to this View. 
+	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	 * @memberOf controller.main
+	 */
+	getControllerName: function() {
+		return "zzk.test.controller.main";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	 * Since the Controller is given to this method, its event handlers can be attached right away.
+	 * @memberOf controller.main
+	 */
+	createContent: function(oController) {
+		var oPage = new sap.m.Page({
+			title: "{i18n>title}",
+			content: []
+		});
+
+		var app = new sap.m.App("myApp", {
+			initialPage: "oPage"
+		});
+		app.addPage(oPage);
+		return app;
+	}
+
+});
